61344f3fa4debe36aa3c4fd1db54cdb331772787
Isaac Kipngeno <isaackipngeno@HP-250-15-6-inch-G9-Notebook-PC-72532965.localdomain>
2024-02-22 08:22:54 +0300

    update customer model

--- models/customer.go
+++ models/customer.go
@@ -1,9 +1,6 @@
 package models
 
-import "gorm.io/gorm"
-
 type Customer struct {
-	gorm.Model
 	Id       int    `json:"id" gorm:"primary_key"`
 	Username string `json:"username"`
 	Phone    string `json:"phone"`
